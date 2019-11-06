9212ed16903966ca109fef141ac9ec6cc0c5903d
Vincent Povirk <vincent@codeweavers.com>
2019-11-06 15:03:27 -0600

    System.Xaml: Add SRID class.

---
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/SRID.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Resources;
+
+partial class SRID
+{
+    public static ResourceManager ResourceManager = new System.Resources.ResourceManager("SR", Assembly.GetExecutingAssembly());
+}
