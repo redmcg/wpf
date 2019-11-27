b66e9fb37b19b04a4ca078198529586d4653160e
Vincent Povirk <vincent@codeweavers.com>
2019-11-27 14:13:41 -0600

    DirectWriteForwarder: Add DWriteMatrix struct.

---
+++ src/Microsoft.DotNet.Wpf/src/DirectWriteForwarder/CPP/DWriteWrapper/DWriteMatrix.cs
@@ -0,0 +1,43 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace MS.Internal.Text.TextInterface
+{
+    /// <summary>
+    /// The DWRITE_MATRIX structure specifies the graphics transform to be applied
+    /// to rendered glyphs.
+    /// </summary>
+    public struct DWriteMatrix
+    {
+        /// <summary>
+        /// Horizontal scaling / cosine of rotation
+        /// </summary>
+        public float M11;
+
+        /// <summary>
+        /// Horizontal shear / sine of rotation
+        /// </summary>
+    	public float M12;
+
+        /// <summary>
+        /// Vertical shear / negative sine of rotation
+        /// </summary>
+        public float M21;
+
+        /// <summary>
+        /// Vertical scaling / cosine of rotation
+        /// </summary>
+        public float M22;
+
+        /// <summary>
+        /// Horizontal shift (always orthogonal regardless of rotation)
+        /// </summary>
+        public float Dx;
+
+        /// <summary>
+        /// Vertical shift (always orthogonal regardless of rotation)
+        /// </summary>
+        public float Dy;
+    }
+}
