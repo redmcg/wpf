a02ea7e9cc6f7e425ffc735f7260fb4a6b51e940
Vincent Povirk <vincent@codeweavers.com>
2019-11-27 10:42:43 -0600

    DirectWriteForwarder: Add GlyphOffset struct.

---
+++ src/Microsoft.DotNet.Wpf/src/DirectWriteForwarder/CPP/DWriteWrapper/GlyphOffset.cs
@@ -0,0 +1,12 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace MS.Internal.Text.TextInterface
+{
+    public struct GlyphOffset
+    {
+        public int du;
+        public int dv;
+    }
+}
