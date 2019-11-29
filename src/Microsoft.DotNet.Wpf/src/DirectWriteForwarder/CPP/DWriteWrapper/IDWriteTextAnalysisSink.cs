e92ff9d9630e2c21c44488623e5987d74324942a
Vincent Povirk <vincent@codeweavers.com>
2019-11-29 13:30:11 -0600

    DirectWriteForwarder: Add IDWriteTextAnalysisSink iface.

---
+++ src/Microsoft.DotNet.Wpf/src/DirectWriteForwarder/CPP/DWriteWrapper/IDWriteTextAnalysisSink.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace MS.Internal.Text.TextInterface
+{
+	[Guid ("5810cd44-0ca0-4701-b3fa-bec5182ae4f6")]
+	[InterfaceType (ComInterfaceType.InterfaceIsIUnknown)]
+	[ComImport ()]
+	internal interface IDWriteTextAnalysisSink
+	{
+		[MethodImpl (MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void SetScriptAnalysis(
+			uint position,
+			uint length,
+			ref DWriteScriptAnalysis scriptanalysis);
+
+		[MethodImpl (MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void SetLineBreakpoints(
+			uint position,
+			uint length,
+			ref byte breakpoints); /* DWRITE_LINE_BREAKPOINT const* */
+
+		[MethodImpl (MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void SetBidiLevel(
+			uint position,
+			uint length,
+			byte explicitLevel,
+			byte resolvedLevel);
+
+		[MethodImpl (MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+		void SetNumberSubstitution(
+			uint position,
+			uint length,
+			[In, MarshalAs(UnmanagedType.Interface)] IDWriteNumberSubstitution substitution);
+	}
+}
