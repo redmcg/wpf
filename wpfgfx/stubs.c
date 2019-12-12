1acf22fedf8abcc90ed200462235aafe89c0dc5f
Vincent Povirk <vincent@codeweavers.com>
2019-12-12 11:02:55 -0600

    wpfgfx: Add stub dll.

---
+++ wpfgfx/stubs.c
@@ -0,0 +1,13 @@
+// stubs.c: stub functions for for WPF
+
+#include <windows.h>
+
+HRESULT WINAPI VisualTarget_AttachToHwnd(HWND hwnd)
+{
+	return S_OK;
+}
+
+HRESULT WINAPI VisualTarget_DetachFromHwnd(HWND hwnd)
+{
+	return S_OK;
+}
