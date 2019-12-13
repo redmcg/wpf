fc8fae84124b6f2ddab9fd1bf43976194575f6db
Vincent Povirk <vincent@codeweavers.com>
2019-12-13 11:14:25 -0600

    wpfgfx: Beginnings of MilResource_SendCommand implementation.

--- wpfgfx/channel.c
+++ wpfgfx/channel.c
@@ -1,10 +1,7 @@
 // channel.c - Connection/Channel functions
 
 #include <windows.h>
-
-typedef struct _MilChannel {
-	void* transport;
-} MilChannel;
+#include "wpfgfx_private.h"
 
 HRESULT WINAPI WgxConnection_Create(BOOL requestSynchronousTransport,
 	void** ppConnection)
@@ -30,5 +27,42 @@ HRESULT WINAPI MilConnection_CreateChannel(void* pTransport, MilChannel* referen
 		return E_OUTOFMEMORY;
 
 	(*result)->transport = pTransport;
+	return S_OK;
+}
+
+#define VALIDATE_STRUCT(t, st) case t: if (size < sizeof(st)) return E_INVALIDARG; break
+
+static HRESULT validate_command(BYTE* data, UINT size)
+{
+	if (!data)
+		return E_POINTER;
+	
+	if (size < 4)
+		return E_INVALIDARG;
+	
+	switch (*(MILCMD*)data)
+	{
+	VALIDATE_STRUCT(MilCmdPartitionNotifyPolicyChangeForNonInteractiveMode,
+		MILCMD_PARTITION_NOTIFYPOLICYCHANGEFORNONINTERACTIVEMODE);
+	default:
+		break;
+	}
+
+	return S_OK;
+}
+
+HRESULT WINAPI MilResource_SendCommand(BYTE* data, UINT size, BOOL sendInSeparateBatch, MilChannel* channel)
+{
+	HRESULT hr;
+
+	if (!channel)
+		return E_POINTER;
+
+	hr = validate_command(data, size);
+	if (FAILED(hr))
+		return hr;
+
+	/* TODO: Save command to batch */
+
 	return S_OK;
 }
