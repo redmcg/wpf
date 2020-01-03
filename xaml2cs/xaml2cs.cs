6aa8c2c338959f067753cdfb2bb039d2c43379f5
Vincent Povirk <vincent@codeweavers.com>
2020-01-03 14:53:16 -0600

    xaml2cs: Add ContentPresenter class.

--- xaml2cs/xaml2cs.cs
+++ xaml2cs/xaml2cs.cs
@@ -15,6 +15,7 @@ class Xaml2Cs
 		types["Button"] = new XamlType("System.Windows.Controls", "Button");
 		types["Canvas"] = new XamlType("System.Windows.Controls", "Canvas");
 		types["Color"] = new XamlType("System.Windows.Media", "Color");
+		types["ContentPresenter"] = new XamlType("System.Windows.Controls", "ContentPresenter");
 		types["Control"] = new XamlType("System.Windows.Controls", "Control");
 		types["ControlTemplate"] = new XamlType("System.Windows.Controls", "ControlTemplate");
 		types["DependencyProperty"] = new XamlType("System.Windows", "DependencyProperty");
