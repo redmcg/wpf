458cf6b4fd717430607270c0a660a2a37540cae8
Vincent Povirk <vincent@codeweavers.com>
2020-01-08 15:00:42 -0600

    xaml2cs: Add FrameworkElement.MaxWidth property.

--- xaml2cs/xaml2cs.cs
+++ xaml2cs/xaml2cs.cs
@@ -216,6 +216,7 @@ class Xaml2Cs
 		types["FrameworkElement"].AddProperty(types["double"], "Height", true);
 		types["FrameworkElement"].AddProperty(types["HorizontalAlignment"], "HorizontalAlignment", false);
 		types["FrameworkElement"].AddProperty(types["Thickness"], "Margin", true);
+		types["FrameworkElement"].AddProperty(types["double"], "MaxWidth", true);
 		types["FrameworkElement"].AddProperty(types["string"], "Name", true);
 		types["FrameworkElement"].AddProperty(types["ResourceDictionary"], "Resources", false);
 		types["FrameworkElement"].props["Resources"].auto = true;
