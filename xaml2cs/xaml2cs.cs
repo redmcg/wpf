8edc94ed7bfa91b56527f333ce8fbb8641181395
Vincent Povirk <vincent@codeweavers.com>
2020-01-08 10:24:18 -0600

    xaml2cs: Add MenuItem.IsCheckable property.

--- xaml2cs/xaml2cs.cs
+++ xaml2cs/xaml2cs.cs
@@ -208,6 +208,7 @@ class Xaml2Cs
 		types["LinearGradientBrush"].AddProperty(types["Point"], "EndPoint", false);
 		types["LinearGradientBrush"].AddProperty(types["Point"], "StartPoint", false);
 		types["Menu"].AddProperty(types["bool"], "IsMainMenu", true);
+		types["MenuItem"].AddProperty(types["bool"], "IsCheckable", true);
 		types["MultiTrigger"].AddProperty(types["ConditionCollection"], "Conditions", false);
 		types["MultiTrigger"].props["Conditions"].auto = true;
 		types["object"].AddProperty(types["object"], "_key", false);
