611419e879c1ec08c6985263512257e426980854
Vincent Povirk <vincent@codeweavers.com>
2020-01-09 13:28:49 -0600

    PresentationUI: Stub Resources type.

---
+++ src/Microsoft.DotNet.Wpf/src/PresentationUI/MS/Internal/documents/Resources.cs
@@ -0,0 +1,42 @@
+
+using System;
+using System.Drawing;
+using System.Resources;
+
+namespace MS.Internal.Documents
+{
+	internal static class Resources
+	{
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				throw new NotImplementedException("MS.Internal.Resources.ResourceManager");
+			}
+		}
+
+		internal static Icon RMPublishingRemove
+		{
+			get
+			{
+				throw new NotImplementedException("MS.Internal.Resources.RMPublishingRemove");
+			}
+		}
+
+		internal static Icon RMPublishingEveryone
+		{
+			get
+			{
+				throw new NotImplementedException("MS.Internal.Resources.RMPublishingEveryone");
+			}
+		}
+
+		internal static Icon RMPublishingPeoplePicker
+		{
+			get
+			{
+				throw new NotImplementedException("MS.Internal.Resources.RMPublishingPeoplePicker");
+			}
+		}
+	}
+}
