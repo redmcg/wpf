0e94e62970fb7273102e70739b6aa092fc39165b
Vincent Povirk <vincent@codeweavers.com>
2020-03-09 13:24:09 -0500

    WpfGfx: Stub modifiers needed by dynarrayimpl.h.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -10,7 +10,9 @@
 #define __deref_in_range(x,y)
 #define __deref_inout_ecount(x)
 #define __deref_opt_out
+#define __deref_opt_out_xcount_part(x,y)
 #define __deref_out
+#define __deref_out_bcount_part(x,y)
 #define __deref_out_ecount(x)
 #define __deref_out_range(x,y)
 #define __drv_aliasesMem
@@ -20,6 +22,8 @@
 #define __drv_sameIRQL
 #define __ecount(x)
 #define __ecount_opt(x)
+#define __field_bcount(x)
+#define __field_range(x,y)
 #define __if_exists(x) if (1)
 #define __in
 #define __in_bcount(x)
@@ -30,6 +34,7 @@
 #define __in_range(x,y)
 #define __in_xcount(x)
 #define __inout
+#define __inout_bcount_part_opt(x,y)
 #define __inout_ecount(x)
 #define __inout_ecount_opt(x)
 #define __out
@@ -37,6 +42,7 @@
 #define __out_bcount_full(x)
 #define __out_ecount(x)
 #define __out_ecount_opt(x)
+#define __out_range(x,y)
 #define __out_xcount(x)
 #define __out_xcount_opt(x)
 #define __post_invalid
