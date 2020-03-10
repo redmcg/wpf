80ee4555fe79070c27e0fb239ba5a6bf65a44ebd
Vincent Povirk <vincent@codeweavers.com>
2020-03-10 14:10:17 -0500

    WpfGfx: Disable most modifier stubs for clang.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -2,6 +2,16 @@
 #pragma once
 
 #ifndef _MSC_VER
+
+#define __deref_out
+#define __drv_functionClass(x)
+#define __drv_sameIRQL
+#define __in
+#define __in_ecount_opt(x)
+#define __out
+#define __post_invalid
+
+#ifndef __clang__
 #define __allocator
 #define __annotation(...)
 #define __bcount(x)
@@ -12,7 +22,6 @@
 #define __deref_opt_out
 #define __deref_opt_inout_ecount_opt(x)
 #define __deref_opt_out_xcount_part(x,y)
-#define __deref_out
 #define __deref_out_bcount_part(x,y)
 #define __deref_out_ecount(x)
 #define __deref_out_ecount_opt(x)
@@ -21,18 +30,14 @@
 #define __drv_aliasesMem
 #define __drv_allocatesMem(x)
 #define __drv_freesMem(x)
-#define __drv_functionClass(x)
-#define __drv_sameIRQL
 #define __ecount(x)
 #define __ecount_opt(x)
 #define __field_bcount(x)
 #define __field_range(x,y)
 #define __if_exists(x) if (1)
-#define __in
 #define __in_bcount(x)
 #define __in_bcount_opt(x)
 #define __in_ecount(x)
-#define __in_ecount_opt(x)
 #define __in_opt
 #define __in_range(x,y)
 #define __in_xcount(x)
@@ -41,7 +46,6 @@
 #define __inout_ecount(x)
 #define __inout_ecount_opt(x)
 #define __inout_ecount_part_opt(x,y)
-#define __out
 #define __out_bcount(x)
 #define __out_bcount_full(x)
 #define __out_ecount(x)
@@ -51,7 +55,8 @@
 #define __out_range(x,y)
 #define __out_xcount(x)
 #define __out_xcount_opt(x)
-#define __post_invalid
 #define __success(x)
 #define _Post_satisfies_(x)
-#endif
+#endif // !defined(__clang__)
+
+#endif // !defined(_MSC_VER)
