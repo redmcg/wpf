9b448b6f7d5cbd6b8d9afc3493a02ea720eb381e
Vincent Povirk <vincent@codeweavers.com>
2020-04-10 15:57:50 -0500

    WpfGfx: Stub __field_bcount_part.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -27,6 +27,7 @@
 #define __drv_sameIRQL
 #define __ecount_opt(x)
 #define __field_bcount(x)
+#define __field_bcount_part(x,y)
 #define __field_ecount_full(x)
 #define __field_ecount_full_opt(x)
 #define __field_ecount_part(x,y)
