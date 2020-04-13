76479c20dccc2a0ba42e2cd9285c71bada0b5c2e
Vincent Povirk <vincent@codeweavers.com>
2020-04-13 10:57:39 -0500

    WpfGfx: Add stub for __deref_out_opt.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -27,6 +27,7 @@
 #define __deref_out_ecount_full(x)
 #define __deref_out_ecount_opt(x)
 #define __deref_out_ecount_part(x,y)
+#define __deref_out_opt
 #define __deref_out_range(x,y)
 #define __deref_out_xcount_full(x)
 #define __deref_out_xcount_opt(x)
