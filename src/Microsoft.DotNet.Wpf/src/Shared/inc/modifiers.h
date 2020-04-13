5c0b7a4d70db76bb44900e0c43a8953a7b16159e
Vincent Povirk <vincent@codeweavers.com>
2020-04-12 22:42:18 -0500

    WpfGfx: Add stub for __field_bcount_opt.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -32,6 +32,7 @@
 #define __drv_sameIRQL
 #define __ecount_opt(x)
 #define __field_bcount(x)
+#define __field_bcount_opt(x)
 #define __field_bcount_part(x,y)
 #define __field_ecount(x)
 #define __field_ecount_full(x)
