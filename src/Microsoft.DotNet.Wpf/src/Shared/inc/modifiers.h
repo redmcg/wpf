c3efc276aa45bf5ed71f4c82fc6dc0e6f8bed85b
Vincent Povirk <vincent@codeweavers.com>
2020-04-30 22:18:29 -0500

    WpfGfx: Add stub for __inout_xcount.

--- src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
+++ src/Microsoft.DotNet.Wpf/src/Shared/inc/modifiers.h
@@ -59,6 +59,7 @@
 #define __inout_ecount(x)
 #define __inout_ecount_opt(x)
 #define __inout_ecount_part_opt(x,y)
+#define __inout_xcount(x)
 #define __kernel_entry
 #define __notnull
 #define __nullterminated
